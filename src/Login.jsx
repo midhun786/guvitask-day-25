2e38b78010cfb59d54b4d4fb66887c5788ff9d22
midhun786 <midhunkumarengineer98@gmail.com>
2022-10-11 19:21:24 +0530

    final

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,88 +1,132 @@
+import axios from 'axios';
 import { Button } from 'bootstrap';
+import { useFormik } from 'formik';
 import React, { useContext, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import { env } from './config';
 import UserContext from './Usercontext';
 
 function Login() {
-   let context=useContext(UserContext);
-   const [use,setUse]=useState("");
+    let context=useContext(UserContext);
+    let navigate = useNavigate();
+  //   let username = "abc";
+  //   let pass = "123";
+  //   let login = () => {
+  //     if (username == "abc" && pass == "123") {
+  //         navigate("/portal/dashboard");
+  //     } else {
+  //       alert("Worng data");
+  //     }
+  //   };
+  
+    let formik = useFormik({
+      initialValues: {
+        email: "",
+        password: "",
+      },
+      validate: (values) => {
+        let errors = {};
+       
+        if (values.email === "") {
+          errors.email= "Please enter email"
+        }
+        if (values.password === "") {
+          errors.password = "Please enter password"
+        }
+        
+        return errors;
+      }, 
      
-    let navigate= useNavigate();
-
-    let username="abc";
-    let pass="admin123"
-    let login=()=>{
-      if(username=="abc" && pass=="admin123"){
-      navigate("/portal");
-      context.setUsername(use)
-      }else{
-        alert("wrong Creadentials")
-      }
-   }
-  return (
-    
-   <>
-
-{/* <!-- Outer Row --> */}
-<div class="row justify-content-center">
-
-    <div class="col-xl-10 col-lg-12 col-md-9">
-
-        <div class="card o-hidden border-0 shadow-lg my-5">
-            <div class="card-body p-0">
+      onSubmit: async (values) => {
+        try {
+         let loginData= await axios.post(`${env.api}/login`, values);
+         console.log(loginData)
+        
+        //  if(loginData.data.token){
+  
+          if(loginData.status === 200){
+            navigate("/portal/dashboard");
+            // window.localStorage.setItem("app-token",loginData.data.token)
+           
+         }else{
+          alert(loginData.data.message)
+         }
+   
+        } catch (error) {
+          alert(error.response.data.message)
+          console.log(error);
+        }
+      },
+  
+    });
+    return (
+      <div className="container">
+        {/* <!-- Outer Row --> */}
+        <div className="row justify-content-center">
+          <div className="col-xl-10 col-lg-12 col-md-9">
+            <div className="card o-hidden border-0 shadow-lg my-5">
+              <div className="card-body p-0">
                 {/* <!-- Nested Row within Card Body --> */}
-                <div class="row">
-                    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                    <div class="col-lg-6">
-                        <div class="p-5">
-                            <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                            </div>
-                            <form class="user">
-                                <div class="form-group">
-                                    <input type="email" class="form-control form-control-user"
-                                        id="exampleInputEmail" aria-describedby="emailHelp"
-                                       value={use} onChange={(e)=>{setUse(e.target.value)}} placeholder="Enter Email Address..."/>
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control form-control-user"
-                                       id="exampleInputPassword" placeholder="Password"/>
-                                </div>
-                                <div class="form-group">
-                                    <div class="custom-control custom-checkbox small">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck"/>
-                                        <label class="custom-control-label" for="customCheck">Remember
-                                            Me</label>
-                                    </div>
-                                </div>
-                                <button onClick={()=>login()} class="btn btn-primary btn-user btn-block">
-                                    Login
-                                </button>
-                                <hr/>
-                                <Link to="/portal" class="btn btn-google btn-user btn-block">
-                                    <i class="fab fa-google fa-fw"></i> Login with Google
-                                </Link>
-                                <Link to="/portal" class="btn btn-facebook btn-user btn-block">
-                                    <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                </Link>
-                            </form>
-                            <hr/>
-                            <div class="text-center">
-                                <a class="small" href="forgot-password.html">Forgot Password?</a>
-                            </div>
-                            <div class="text-center">
-                                <a class="small" href="register.html">Create an Account!</a>
-                            </div>
+                <div className="row">
+                  <div className="col-lg-6 mx-auto">
+                    <div className="p-5">
+                      <div className="text-center">
+                        <h1 className="h4 text-gray-900 mb-4">Welcome To Login Page!</h1>
+                      </div>
+                      <form className="user" onSubmit={formik.handleSubmit}>
+                      <div className="form-group">      
+                          <input
+                            className={`form-control ${formik.errors.email ? `input-error` : ``}`}
+                            id="exampleInputEmail"
+                            type={"email"}
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            name="email"
+                            placeholder="Enter Email Address..."
+                          />
+                           <span style={{ color: "red" }}>{formik.errors.email}</span>
                         </div>
+                        <div className="form-group">
+                          <input
+                           className={`form-control ${formik.errors.password ? `input-error` : ``}`}
+                            id="exampleInputPassword"
+                            type={"password"}
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            placeholder="Password"
+                            name="password"
+                          />
+                          <span style={{ color: "red" }}>{formik.errors.password}</span>
+                        </div>
+                        <div className="form-group">
+                         
+                         
+                          <div className='text-end  fw-bold '>
+                     <Link to={'/forgotpassword'} className=' '>Forgot password</Link>
+                      
+                      </div>
+                        </div>
+  
+                        <button
+                          type="submit"
+                          className="btn btn-primary btn-user fw-bold btn-block"
+                        >
+                         LOGIN
+                        </button>
+                      </form>
+                      <div className='text-center p-3 fw-bold mt-2'>
+                      <p>Don't have an Account? <Link to={'/register'} className='btn btn-outline-info'>Register</Link></p>
+                      
+                      </div>
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-
-    </div>
-</div>
-    </>
-  )
-}
-
-export default Login;
+      </div>
+    );
+  }
+  
+  export default Login;
